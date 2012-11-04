30003b83604dbd052af8c1e245506609a9073687
Simon Cropp <simon.cropp@gmail.com>
2012-11-04 17:19:34 +1100

    fix version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Freezable")]
 [assembly: AssemblyProduct("Freezable")]
-[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
