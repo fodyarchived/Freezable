0acf06c6a6c4c39c60a4bde4d958e553a289b849
Simon Cropp <simon.cropp@gmail.com>
2013-06-16 18:48:21 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Freezable")]
 [assembly: AssemblyProduct("Freezable")]
-[assembly: AssemblyVersion("1.6.1.1")]
-[assembly: AssemblyFileVersion("1.6.1.1")]
+[assembly: AssemblyVersion("1.6.1.2")]
+[assembly: AssemblyFileVersion("1.6.1.2")]
