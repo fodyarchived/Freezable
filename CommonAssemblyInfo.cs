14c52a6b0eeba177e7ec1185ad539ade418827bc
Simon Cropp <simon.cropp@gmail.com>
2013-07-14 19:08:27 +1000

    Update CommonAssemblyInfo.cs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Freezable")]
 [assembly: AssemblyProduct("Freezable")]
-[assembly: AssemblyVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyVersion("1.6.4")]
+[assembly: AssemblyFileVersion("1.6.4")]
