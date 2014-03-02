ccbd591fe0efef5e066f3a0fc07e41d58ff7df49
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:13:21 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Freezable")]
 [assembly: AssemblyProduct("Freezable")]
-[assembly: AssemblyVersion("1.6.6")]
-[assembly: AssemblyFileVersion("1.6.6")]
+[assembly: AssemblyVersion("1.6.7")]
+[assembly: AssemblyFileVersion("1.6.7")]
