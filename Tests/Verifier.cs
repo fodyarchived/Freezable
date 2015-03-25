d6c0dc88cbcadf73a59a2eaaaea545617e1413ee
Simon Cropp <simon.cropp@gmail.com>
2015-03-26 10:53:31 +1100

    refs

--- Tests/Verifier.cs
+++ Tests/Verifier.cs
@@ -1,22 +1,47 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
-using Microsoft.Build.Utilities;
-using NUnit.Framework;
+
 
 public static class Verifier
 {
+    static string exePath;
+    static bool peverifyFound = true;
+
+    static Verifier()
+    {
+
+        exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
+
+        if (!File.Exists(exePath))
+        {
+            exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
+        }
+        peverifyFound = File.Exists(exePath);
+        if (!peverifyFound)
+        {
+#if(!DEBUG)
+            throw new Exception("Could not fund PEVerify");
+#endif
+        }
+    }
     public static void Verify(string beforeAssemblyPath, string afterAssemblyPath)
     {
+        if (!peverifyFound)
+        {
+            return;
+        }
+        Debug.WriteLine(afterAssemblyPath);
         var before = Validate(beforeAssemblyPath);
         var after = Validate(afterAssemblyPath);
         var message = string.Format("Failed processing {0}\r\n{1}", Path.GetFileName(afterAssemblyPath), after);
-        Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
+        NUnit.Framework.Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
     }
 
     public static string Validate(string assemblyPath2)
     {
-        var exePath = GetPathToPEVerify();
+
         var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
         {
             RedirectStandardOutput = true,
@@ -28,10 +53,6 @@ public static class Verifier
         return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
     }
 
-    static string GetPathToPEVerify()
-    {
-        return Path.Combine(ToolLocationHelper.GetPathToDotNetFrameworkSdk(TargetDotNetFrameworkVersion.Version40), @"bin\NETFX 4.0 Tools\peverify.exe");
-    }
     static string TrimLineNumbers(string foo)
     {
         return Regex.Replace(foo, @"0x.*]", "");
